cf918613db55dbdab72166b1d8613b3e37d23bed
kstudilova <kstudilova@gmail.com>
2026-09-17 03:29:28 +0300

    fix clicker clock

--- clicker.hpp
+++ clicker.hpp
@@ -7,21 +7,21 @@ class Clicker
 {
   public:
     Clicker():
-      start_(std::chrono::high_resolution_clock::now())
+      start_(std::chrono::steady_clock::now())
     {}
 
     double millisec() const
     {
-      using std::chrono::high_resolution_clock;
+      using std::chrono::steady_clock;
       using std::chrono::duration_cast;
       using std::chrono::milliseconds;
 
-      auto t = high_resolution_clock::now();
+      auto t = steady_clock::now();
       return duration_cast< milliseconds >(t - start_).count();
     }
 
   private:
-    std::chrono::time_point<std::chrono::high_resolution_clock> start_;
+    std::chrono::time_point< std::chrono::steady_clock > start_;
 };
 
 #endif
